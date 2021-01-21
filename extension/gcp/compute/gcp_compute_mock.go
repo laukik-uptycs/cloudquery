68fb0cc44447fe2866cc6b06afafd506b3151f74
Gopal Bonthu <62247609+gbonthu@users.noreply.github.com>
2021-01-21 14:51:55 -0500

    Add placeholder mock methods for gcp (#30)

--- extension/gcp/compute/gcp_compute_mock.go
+++ extension/gcp/compute/gcp_compute_mock.go
@@ -3,6 +3,7 @@ package compute
 import (
 	"context"
 
+	"google.golang.org/api/compute/v1"
 	gcpcompute "google.golang.org/api/compute/v1"
 	"google.golang.org/api/option"
 )
@@ -116,4 +117,88 @@ func (gcp *GcpComputeMock) AddNetworks(inList []*gcpcompute.Network) {
 
 func (gcp *GcpComputeMock) ClearNetworks() {
 	gcp.networksPage.Items = make([]*gcpcompute.Network, 0)
+}
+
+func (gcp *GcpComputeMock) NewImagesService(svc *gcpcompute.Service) *gcpcompute.ImagesService {
+	return nil
+}
+
+func (gcp *GcpComputeMock) ImagesList(apiSvc *gcpcompute.ImagesService, projectID string) *gcpcompute.ImagesListCall {
+	return nil
+}
+
+func (gcp *GcpComputeMock) ImagesPages(listCall *gcpcompute.ImagesListCall, ctx context.Context, cb callbackImagesPages) error {
+	return nil
+}
+
+func (gcp *GcpComputeMock) NewInterconnectsService(svc *gcpcompute.Service) *gcpcompute.InterconnectsService {
+	return nil
+}
+
+func (gcp *GcpComputeMock) InterconnectsList(apiSvc *gcpcompute.InterconnectsService, projectID string) *gcpcompute.InterconnectsListCall {
+	return nil
+}
+
+func (gcp *GcpComputeMock) InterconnectsPages(listCall *gcpcompute.InterconnectsListCall, ctx context.Context, cb callbackInterconnectsPages) error {
+	return nil
+}
+
+func (gcp *GcpComputeMock) NewRoutesService(svc *gcpcompute.Service) *gcpcompute.RoutesService {
+	return nil
+}
+
+func (gcp *GcpComputeMock) RoutesList(apiSvc *gcpcompute.RoutesService, projectID string) *gcpcompute.RoutesListCall {
+	return nil
+}
+
+func (gcp *GcpComputeMock) RoutesPages(listCall *gcpcompute.RoutesListCall, ctx context.Context, cb callbackRoutesPages) error {
+	return nil
+}
+
+func (gcp *GcpComputeMock) NewReservationsService(svc *gcpcompute.Service) *gcpcompute.ReservationsService {
+	return nil
+}
+
+func (gcp *GcpComputeMock) ReservationsAggregatedList(apiSvc *gcpcompute.ReservationsService, projectID string) *gcpcompute.ReservationsAggregatedListCall {
+	return nil
+}
+
+func (gcp *GcpComputeMock) ReservationsPages(listCall *gcpcompute.ReservationsAggregatedListCall, ctx context.Context, cb callbackReservationsPages) error {
+	return nil
+}
+
+func (gcp *GcpComputeMock) NewRoutersService(svc *gcpcompute.Service) *gcpcompute.RoutersService {
+	return nil
+}
+
+func (gcp *GcpComputeMock) RoutersAggregatedList(apiSvc *gcpcompute.RoutersService, projectID string) *gcpcompute.RoutersAggregatedListCall {
+	return nil
+}
+
+func (gcp *GcpComputeMock) RoutersPages(listCall *gcpcompute.RoutersAggregatedListCall, ctx context.Context, cb callbackRoutersPages) error {
+	return nil
+}
+
+func (gcp *GcpComputeMock) NewVpnTunnelsService(svc *gcpcompute.Service) *gcpcompute.VpnTunnelsService {
+	return nil
+}
+
+func (gcp *GcpComputeMock) VpnTunnelsAggregatedList(apiSvc *gcpcompute.VpnTunnelsService, projectID string) *gcpcompute.VpnTunnelsAggregatedListCall {
+	return nil
+}
+
+func (gcp *GcpComputeMock) VpnTunnelsPages(listCall *gcpcompute.VpnTunnelsAggregatedListCall, ctx context.Context, cb callbackVpnTunnelsPages) error {
+	return nil
+}
+
+func (gcp *GcpComputeMock) NewVpnGatewaysService(svc *gcpcompute.Service) *gcpcompute.VpnGatewaysService {
+	return nil
+}
+
+func (gcp *GcpComputeMock) VpnGatewaysAggregatedList(apiSvc *gcpcompute.VpnGatewaysService, projectID string) *compute.VpnGatewaysAggregatedListCall {
+	return nil
+}
+
+func (gcp *GcpComputeMock) VpnGatewaysPages(listCall *gcpcompute.VpnGatewaysAggregatedListCall, ctx context.Context, cb callbackVpnGatewaysPages) error {
+	return nil
 }
