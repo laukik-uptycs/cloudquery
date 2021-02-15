2982089416b1b66cdb151af839f8c342d69551a2
Manish Kumar <mkumar@uptycs.com>
2021-02-15 10:56:17 +0530

    added apigateway

--- extension/helper.go
+++ extension/helper.go
@@ -124,13 +124,7 @@ func ReadExtensionConfigurations(filePath string, verbose bool) error {
 
 // ReadTableConfigurations TODO
 func ReadTableConfigurations(homeDir string) {
-<<<<<<< HEAD
-	var awsConfigFileList = []string{"aws/apigateway/table_config.json", "aws/ec2/table_config.json", "aws/s3/table_config.json", "aws/iam/table_config.json"}
-=======
-
-	var awsConfigFileList = []string{"aws/acm/table_config.json", "aws/ec2/table_config.json", "aws/s3/table_config.json", "aws/iam/table_config.json", "aws/cloudwatch/table_config.json", "aws/config/table_config.json"}
-
->>>>>>> remotes/origin/master
+	var awsConfigFileList = []string{"aws/acm/table_config.json", "aws/apigateway/table_config.json", "aws/ec2/table_config.json", "aws/s3/table_config.json", "aws/iam/table_config.json", "aws/cloudwatch/table_config.json", "aws/config/table_config.json"}
 	var gcpConfigFileList = []string{
 		"gcp/compute/table_config.json",
 		"gcp/storage/table_config.json",
@@ -181,7 +175,7 @@ func RegisterPlugins(server *osquery.ExtensionManagerServer) {
 	// AWS ACM
 	server.RegisterPlugin(table.NewPlugin("aws_acm_certificate", acm.ListCertificatesColumns(), acm.ListCertificatesGenerate))
 	// AWS APIGATEWAY
-	server.RegisterPlugin(table.NewPlugin("aws_api_gw_rest_api", apigateway.GetRestApisColumns(), apigateway.GetRestApisGenerate))	
+	server.RegisterPlugin(table.NewPlugin("aws_api_gw_rest_api", apigateway.GetRestApisColumns(), apigateway.GetRestApisGenerate))
 	// AWS EC2
 	server.RegisterPlugin(table.NewPlugin("aws_ec2_instance", ec2.DescribeInstancesColumns(), ec2.DescribeInstancesGenerate))
 	server.RegisterPlugin(table.NewPlugin("aws_ec2_vpc", ec2.DescribeVpcsColumns(), ec2.DescribeVpcsGenerate))
