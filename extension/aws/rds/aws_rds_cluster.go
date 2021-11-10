f4eced3397da2383012bc39ddd39814d46efb5fd
laukik-uptycs <lchavan@uptycs.com>
2021-11-10 23:10:35 +0530

    aws-rds-cluster table is added

---
+++ extension/aws/rds/aws_rds_cluster.go
@@ -0,0 +1,155 @@
+package rds
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/Uptycs/cloudquery/utilities"
+
+	"github.com/Uptycs/basequery-go/plugin/table"
+	extaws "github.com/Uptycs/cloudquery/extension/aws"
+	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
+	"github.com/aws/aws-sdk-go-v2/service/rds"
+)
+
+func ListClustersColumns() []table.ColumnDefinition {
+	return []table.ColumnDefinition{
+		table.TextColumn("account_id"),
+		table.TextColumn("region_code"),
+		table.TextColumn("cluster_identifier"),
+		table.TextColumn("engine"),
+		table.TextColumn("subnet_group"),
+		table.TextColumn("multi_AZ"),
+		table.TextColumn("status"),
+	}
+}
+
+func DescribeClustersGenerate(osqCtx context.Context, queryContext table.QueryContext) ([]map[string]string, error) {
+	resultMap := make([]map[string]string, 0)
+	if len(utilities.ExtConfiguration.ExtConfAws.Accounts) == 0 && extaws.ShouldProcessAccount("aws_rds_cluster", utilities.AwsAccountID) {
+		utilities.GetLogger().WithFields(log.Fields{
+			"tableName": "aws_rds_cluster",
+			"account":   "default",
+		}).Info("processing account")
+		results, err := processAccountDescribeClusters(osqCtx, queryContext, nil)
+		if err != nil {
+			return resultMap, err
+		}
+		resultMap = append(resultMap, results...)
+	} else {
+		for _, account := range utilities.ExtConfiguration.ExtConfAws.Accounts {
+			if !extaws.ShouldProcessAccount("aws_rds_cluster", account.ID) {
+				continue
+			}
+			utilities.GetLogger().WithFields(log.Fields{
+				"tableName": "aws_rds_cluster",
+				"account":   account.ID,
+			}).Info("processing account")
+			results, err := processAccountDescribeClusters(osqCtx, queryContext, &account)
+			if err != nil {
+				continue
+			}
+			resultMap = append(resultMap, results...)
+		}
+	}
+
+	return resultMap, nil
+}
+
+func processRegionDescribeClusters(osqCtx context.Context, queryContext table.QueryContext, tableConfig *utilities.TableConfig, account *utilities.ExtensionConfigurationAwsAccount, region types.Region) ([]map[string]string, error) {
+	resultMap := make([]map[string]string, 0)
+	sess, err := extaws.GetAwsConfig(account, *region.RegionName)
+	if err != nil {
+		return resultMap, err
+	}
+
+	accountId := utilities.AwsAccountID
+	if account != nil {
+		accountId = account.ID
+	}
+
+	utilities.GetLogger().WithFields(log.Fields{
+		"tableName": "aws_rds_cluster",
+		"account":   accountId,
+		"region":    *region.RegionName,
+	}).Debug("processing region")
+
+	svc := rds.NewFromConfig(*sess)
+	params := &rds.DescribeDBClustersInput{}
+
+	paginator := rds.NewDescribeDBClustersPaginator(svc, params)
+
+	for {
+		page, err := paginator.NextPage(osqCtx)
+		if err != nil {
+			utilities.GetLogger().WithFields(log.Fields{
+				"tableName": "aws_rds_cluster",
+				"account":   accountId,
+				"region":    *region.RegionName,
+				"task":      "ListCertificates",
+				"errString": err.Error(),
+			}).Error("failed to process region")
+			return resultMap, err
+		}
+		byteArr, err := json.Marshal(page)
+		if err != nil {
+			utilities.GetLogger().WithFields(log.Fields{
+				"tableName": "aws_rds_cluster",
+				"account":   accountId,
+				"region":    *region.RegionName,
+				"task":      "ListCertificates",
+				"errString": err.Error(),
+			}).Error("failed to marshal response")
+			return nil, err
+		}
+		table := utilities.NewTable(byteArr, tableConfig)
+		for _, row := range table.Rows {
+			if !extaws.ShouldProcessRow(osqCtx, queryContext, "aws_rds_cluster", accountId, *region.RegionName, row) {
+				continue
+			}
+			result := extaws.RowToMap(row, accountId, *region.RegionName, tableConfig)
+			resultMap = append(resultMap, result)
+		}
+		if !paginator.HasMorePages() {
+			break
+		}
+	}
+	return resultMap, nil
+}
+
+func processAccountDescribeClusters(osqCtx context.Context, queryContext table.QueryContext, account *utilities.ExtensionConfigurationAwsAccount) ([]map[string]string, error) {
+	resultMap := make([]map[string]string, 0)
+	awsSession, err := extaws.GetAwsConfig(account, "us-east-1")
+	if err != nil {
+		return resultMap, err
+	}
+	regions, err := extaws.FetchRegions(osqCtx, awsSession)
+	if err != nil {
+		return resultMap, err
+	}
+	tableConfig, ok := utilities.TableConfigurationMap["aws_rds_cluster"]
+	if !ok {
+		utilities.GetLogger().WithFields(log.Fields{
+			"tableName": "aws_rds_cluster",
+		}).Error("failed to get table configuration")
+		return resultMap, fmt.Errorf("table configuration not found")
+	}
+	for _, region := range regions {
+		accountId := utilities.AwsAccountID
+		if account != nil {
+			accountId = account.ID
+		}
+		if !extaws.ShouldProcessRegion("aws_rds_cluster", accountId, *region.RegionName) {
+			continue
+		}
+		result, err := processRegionDescribeClusters(osqCtx, queryContext, tableConfig, account, region)
+		if err != nil {
+			continue
+		}
+		resultMap = append(resultMap, result...)
+	}
+	return resultMap, nil
+}
