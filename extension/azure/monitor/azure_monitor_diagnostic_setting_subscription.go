5af48f0c52fe0d67f5ac5062631a62770fcd2aec
laukik-uptycs <91864935+laukik-uptycs@users.noreply.github.com>
2021-12-30 09:14:31 +0530

    Azure Monitor Diagnostic Settings Subscription (#111)
    * new table diagnostic settings subscription is added
    
    * subscription api is chnaged to use appropriate format

--- extension/azure/monitor/azure_monitor_diagnostic_setting_subscription.go
+++ extension/azure/monitor/azure_monitor_diagnostic_setting_subscription.go
@@ -14,48 +14,46 @@ import (
 	"github.com/Uptycs/cloudquery/utilities"
 
 	azuremonitor "github.com/Azure/azure-sdk-for-go/services/preview/monitor/mgmt/2021-07-01-preview/insights"
+
+	azurestorage "github.com/Uptycs/cloudquery/extension/azure/storage"
 )
 
-const azureMonitorDiagnosticSettingsResource = "azure_monitor_diagnostic_settings_resource"
+const azureMonitorDiagnosticSettingsSubscription = "azure_monitor_diagnostic_settings_resource"
 
-// DiagnosticSettingsResourceColumns returns the list of columns in the table
-func DiagnosticSettingsResourceColumns() []table.ColumnDefinition {
+// DiagnosticSettingsSubscriptionColumns returns the list of columns in the table
+func DiagnosticSettingsSubscriptionColumns() []table.ColumnDefinition {
 	return []table.ColumnDefinition{
-		table.TextColumn("id"),
-		table.TextColumn("name"),
-		// table.TextColumn("properties"),
 		table.TextColumn("event_hub_authorization_rule_id"),
 		table.TextColumn("event_hub_name"),
 		table.TextColumn("log_analytics_destination_type"),
 		table.TextColumn("logs"),
-		// table.TextColumn("logs_category"),
-		// table.TextColumn("logs_enabled"),
-		// table.TextColumn("logs_retention_policy"),
+		table.TextColumn("logs_category"),
+		table.TextColumn("logs_enabled"),
+		table.TextColumn("logs_retention_policy"),
 		// table.IntegerColumn("logs_retention_policy_days"),
 		// table.TextColumn("logs_retention_policy_enabled"),
 		table.TextColumn("metrics"),
-		// table.TextColumn("metrics_category"),
-		// table.TextColumn("metrics_enabled"),
-		// table.TextColumn("metrics_retention_policy"),
+		table.TextColumn("metrics_category"),
+		table.TextColumn("metrics_enabled"),
+		table.TextColumn("metrics_retention_policy"),
 		// table.IntegerColumn("metrics_retention_policy_days"),
 		// table.TextColumn("metrics_retention_policy_enabled"),
-		// table.TextColumn("metrics_time_grain"),
+		table.TextColumn("metrics_time_grain"),
 		table.TextColumn("service_bus_rule_id"),
 		table.TextColumn("storage_account_id"),
 		table.TextColumn("workspace_id"),
-		table.TextColumn("type"),
 	}
 }
 
-// DiagnosticSettingsResourceGenerate returns the rows in the table for all configured accounts
-func DiagnosticSettingsResourceGenerate(osqCtx context.Context, queryContext table.QueryContext) ([]map[string]string, error) {
+// DiagnosticSettingsSubscriptionGenerate returns the rows in the table for all configured accounts
+func DiagnosticSettingsSubscriptionGenerate(osqCtx context.Context, queryContext table.QueryContext) ([]map[string]string, error) {
 	resultMap := make([]map[string]string, 0)
 	if len(utilities.ExtConfiguration.ExtConfAzure.Accounts) == 0 {
 		utilities.GetLogger().WithFields(log.Fields{
-			"tableName": azureMonitorDiagnosticSettingsResource,
+			"tableName": azureMonitorDiagnosticSettingsSubscription,
 			"account":   "default",
 		}).Info("processing account")
-		results, err := processDignosticSettingsResource(nil)
+		results, err := processDignosticSettingsSubscription(nil)
 		if err != nil {
 			return resultMap, err
 		}
@@ -63,10 +61,10 @@ func DiagnosticSettingsResourceGenerate(osqCtx context.Context, queryContext tab
 	} else {
 		for _, account := range utilities.ExtConfiguration.ExtConfAzure.Accounts {
 			utilities.GetLogger().WithFields(log.Fields{
-				"tableName": azureMonitorDiagnosticSettingsResource,
+				"tableName": azureMonitorDiagnosticSettingsSubscription,
 				"account":   account.SubscriptionID,
 			}).Info("processing account")
-			results, err := processDignosticSettingsResource(&account)
+			results, err := processDignosticSettingsSubscription(&account)
 			if err != nil {
 				continue
 			}
@@ -77,7 +75,7 @@ func DiagnosticSettingsResourceGenerate(osqCtx context.Context, queryContext tab
 	return resultMap, nil
 }
 
-func processDignosticSettingsResource(account *utilities.ExtensionConfigurationAzureAccount) ([]map[string]string, error) {
+func processDignosticSettingsSubscription(account *utilities.ExtensionConfigurationAzureAccount) ([]map[string]string, error) {
 	resultMap := make([]map[string]string, 0)
 	var wg sync.WaitGroup
 	session, err := extazure.GetAuthSession(account)
@@ -92,33 +90,52 @@ func processDignosticSettingsResource(account *utilities.ExtensionConfigurationA
 
 	wg.Add(len(groups))
 
-	tableConfig, ok := utilities.TableConfigurationMap[azureMonitorDiagnosticSettingsResource]
+	tableConfig, ok := utilities.TableConfigurationMap[azureMonitorDiagnosticSettingsSubscription]
 	if !ok {
 		utilities.GetLogger().WithFields(log.Fields{
-			"tableName": azureMonitorDiagnosticSettingsResource,
+			"tableName": azureMonitorDiagnosticSettingsSubscription,
 		}).Error("failed to get table configuration")
 		return resultMap, fmt.Errorf("table configuration not found")
 	}
 
 	for _, group := range groups {
-		go getDignosticSettingsResource(session, group, &wg, &resultMap, tableConfig)
+		go getStorageAccountIdForSubscription(session, group, &wg, &resultMap, tableConfig)
 	}
 	wg.Wait()
 	return resultMap, nil
 }
-
-func getDignosticSettingsResource(session *extazure.AzureSession, rg string, wg *sync.WaitGroup, resultMap *[]map[string]string, tableConfig *utilities.TableConfig) {
+func getStorageAccountIdForSubscription(session *extazure.AzureSession, rg string, wg *sync.WaitGroup, resultMap *[]map[string]string, tableConfig *utilities.TableConfig) {
 	defer wg.Done()
+
+	diagnosticSettings := make([]azuremonitor.DiagnosticSettingsResource, 0)
+
+	for resourceItr, err := azurestorage.GetStorageAccounts(session, rg); resourceItr.NotDone(); err = resourceItr.Next() {
+		if err != nil {
+			utilities.GetLogger().WithFields(log.Fields{
+				"tableName":     azureMonitorDiagnosticSettingsSubscription,
+				"resourceGroup": rg,
+				"errString":     err.Error(),
+			}).Error("failed to get resource list")
+			continue
+		}
+		resource := resourceItr.Value()
+
+		getDiagnosticSettingSubscription(session, rg, *resource.ID, &diagnosticSettings)
+	}
+
+	addDignosticSettingsSubscription(session, rg, resultMap, tableConfig, diagnosticSettings)
+}
+
+func getDiagnosticSettingSubscription(session *extazure.AzureSession, rg string, resourceURI string, diagnosticSettings *[]azuremonitor.DiagnosticSettingsResource) {
 
 	svcClient := azuremonitor.NewDiagnosticSettingsClient(session.SubscriptionId)
 	svcClient.Authorizer = session.Authorizer
 
-	resourceURI := "/subscriptions/" + session.SubscriptionId
 	resourceItr, err := svcClient.List(context.Background(), resourceURI)
 
 	if err != nil {
 		utilities.GetLogger().WithFields(log.Fields{
-			"tableName":     azureMonitorDiagnosticSettingsResource,
+			"tableName":     azureMonitorDiagnosticSettingsSubscription,
 			"resourceGroup": rg,
 			"errString":     err.Error(),
 		}).Error("failed to get resource list")
@@ -126,13 +143,17 @@ func getDignosticSettingsResource(session *extazure.AzureSession, rg string, wg 
 
 	resource := resourceItr.Value
 
-	for _, diagnosticSetting := range *resource {
+	*diagnosticSettings = append(*diagnosticSettings, *resource...)
+}
+
+func addDignosticSettingsSubscription(session *extazure.AzureSession, rg string, resultMap *[]map[string]string, tableConfig *utilities.TableConfig, diagnosticSettings []azuremonitor.DiagnosticSettingsResource) {
+	for _, diagnosticSetting := range diagnosticSettings {
 		structs.DefaultTagName = "json"
 		resMap := structs.Map(diagnosticSetting)
 		byteArr, err := json.Marshal(resMap)
 		if err != nil {
 			utilities.GetLogger().WithFields(log.Fields{
-				"tableName":     azureMonitorDiagnosticSettingsResource,
+				"tableName":     azureMonitorDiagnosticSettingsSubscription,
 				"resourceGroup": rg,
 				"errString":     err.Error(),
 			}).Error("failed to marshal response")
